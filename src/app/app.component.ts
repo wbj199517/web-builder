b9354f9c34e10ff8d66c9deacbacb24163c5e322
brianwang <brianwang2013@hotmail.com>
2019-10-26 21:50:26 -0400

    web-builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+  <runtime-content></runtime-content>
+  `,
 })
-export class AppComponent {
-  title = 'web-builder';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+
+
+import { RuntimeContentComponent } from './runtime-content.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+
+        RuntimeContentComponent,
+        
+    ],
+    entryComponents: [
+  
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
